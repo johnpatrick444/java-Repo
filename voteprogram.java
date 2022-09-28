3fde8cc4358b60f171c8bbc688aac97f2ebdbc21
johnpatrick444 <114388964+johnpatrick444@users.noreply.github.com>
2022-09-28 09:40:30 +0530

    Add files via upload

---
+++ voteprogram.java
@@ -0,0 +1,22 @@
+package sub1;
+//lines added to see changes made...
+public class voteprogram {
+
+	public static void main(String[] args) {
+
+	int age=20;
+	if(age>18)
+	{
+		System.out.print("age is greater than 18 and aproved to vote");
+	}
+
+	else 
+	{
+		System.out.print("note qualified to vote ");
+	}
+
+
+	}
+}
+
+
